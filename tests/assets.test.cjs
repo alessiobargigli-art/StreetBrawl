d0f51f4cebe8804eba3ddc26578304433f58b5af
alessiobargigli-art <alessio.bargigli@gmail.com>
2026-09-18 08:27:13 +0200

    test(assets): fail deterministically on required image decode errors

---
+++ tests/assets.test.cjs
@@ -0,0 +1,10 @@
+const test=require('node:test');const assert=require('node:assert/strict');
+global.window=global;global.requestAnimationFrame=()=>1;global.cancelAnimationFrame=()=>{};
+class BadImage{constructor(){this.decoding='';this.naturalWidth=0;this.naturalHeight=0;}set src(v){this._src=v}decode(){return Promise.reject(new Error('decode failed'))}}
+global.Image=BadImage;
+const {CoopGame}=require('../.test-game/online/CoopGame.js');
+class Client extends EventTarget{sendInput(){}}
+test('art preload rejects a missing/undecodable required image instead of starting silently',async()=>{
+ const canvas={getContext:()=>({imageSmoothingEnabled:false})};const game=new CoopGame(canvas,new Client());
+ await assert.rejects(()=>game.preload(),/non caricabile/);game.stop();
+});
