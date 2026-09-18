75f3b79751a142032689b9406689e0c4525edca7
alessiobargigli-art <alessio.bargigli@gmail.com>
2026-09-18 08:27:24 +0200

    test(assets): provide browser event target mock

--- tests/assets.test.cjs
+++ tests/assets.test.cjs
@@ -1,5 +1,5 @@
 const test=require('node:test');const assert=require('node:assert/strict');
-global.window=global;global.requestAnimationFrame=()=>1;global.cancelAnimationFrame=()=>{};
+global.window=new EventTarget();global.requestAnimationFrame=()=>1;global.cancelAnimationFrame=()=>{};
 class BadImage{constructor(){this.decoding='';this.naturalWidth=0;this.naturalHeight=0;}set src(v){this._src=v}decode(){return Promise.reject(new Error('decode failed'))}}
 global.Image=BadImage;
 const {CoopGame}=require('../.test-game/online/CoopGame.js');
